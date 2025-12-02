1323eb96a0720b55df8b3986586c5a72236176ce
Nikita Borysov <nborysov15@gmail.com>
2025-12-02 02:11:34 +0200

    Add AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EduPlanApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EduPlanApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Таблиці
+        public DbSet<TaskItem> TaskItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Налаштування зв'язків
+            modelBuilder.Entity<TaskItem>()
+                .HasOne(t => t.Category)
+                .WithMany(c => c.Tasks)
+                .HasForeignKey(t => t.CategoryId);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasOne(t => t.Priority)
+                .WithMany(p => p.Tasks)
+                .HasForeignKey(t => t.PriorityId);
+
+            // Виклик методу для заповнення початковими даними
+            DbInitializer.Seed(modelBuilder);
+        }
+    }
+}
