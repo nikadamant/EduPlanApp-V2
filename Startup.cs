28e65ce07de23711c1ebc1207a8ec606f733ed68
Nikita Borysov <nborysov15@gmail.com>
2025-12-02 02:22:10 +0200

    Add DB configuration settings

--- Startup.cs
+++ Startup.cs
@@ -1,42 +1,109 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using EduPlanApp.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using EduPlanApp.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EduPlanApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         private const string GoogleClientId = "158597371582-e8ihcgl7mfcnin484oreu8mbuko681r1.apps.googleusercontent.com";
         private const string GoogleClientSecret = "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";
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // 1. Налаштування Identity/Authentication 
-            services.AddAuthentication("Cookies")
-                .AddCookie("Cookies", options => { options.LoginPath = "/Account/Login"; })
+            var dbType = Configuration.GetValue<string>("DatabaseType");
+            var connectionString = "";
 
-                // ДОДАЄМО ПРОВАЙДЕРА GOOGLE (OAuth2)
-                .AddGoogle(options =>
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                switch (dbType)
                 {
-                    options.ClientId = GoogleClientId;
-                    options.ClientSecret = GoogleClientSecret;
-                    options.CallbackPath = "/signin-google";
-                });
+                    case "MsSql":
+                        connectionString = Configuration.GetConnectionString("MsSqlConnection");
+                        options.UseSqlServer(connectionString);
+                        break;
+                    case "Postgres":
+                        connectionString = Configuration.GetConnectionString("PostgresConnection");
+                        options.UseNpgsql(connectionString);
+                        break;
+                    case "Sqlite":
+                        connectionString = Configuration.GetConnectionString("SqliteConnection");
+                        options.UseSqlite(connectionString);
+                        break;
+                    case "InMemory":
+                        options.UseInMemoryDatabase("EduPlanAppInMemoryDB");
+                        break;
+                    default:
+                        throw new ArgumentException($"Невідомий тип бази даних: {dbType}");
+                }
+            });
 
-            // 2. Додавання MVC сервісів
-            services.AddControllersWithViews();
+            // Додаємо DbInitializer як хостовану службу
+            services.AddHostedService<DbServiceInitializer>();
+            services.AddScoped<IUserService, UserService>();
 
-            // 3. Налаштування політики авторизації
+
+            // 1. Налаштування Identity/Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "Google";
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+            })
+            .AddGoogle(options =>
+            {
+                options.ClientId = GoogleClientId;
+                options.ClientSecret = GoogleClientSecret;
+                options.CallbackPath = "/signin-google";
+                options.Scope.Add("profile");
+            });
+
             services.AddAuthorization(options =>
             {
                 options.FallbackPolicy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .Build();
-
             });
+
+            services.AddControllersWithViews();
         }
 
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthentication();
